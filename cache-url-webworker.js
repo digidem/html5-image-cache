df55a0de9b345e599ba93c89985ebc06712aa7a5
Gregor MacLennan <gmaclennan@digital-democracy.org>
2015-12-10 21:53:15 -0800

    Convert to standard

--- cache-url-webworker.js
+++ cache-url-webworker.js
@@ -1,60 +1,61 @@
-module.exports = function() {
+/* eslint-env worker */
 
-    self.requestFileSystemSync = self.webkitRequestFileSystemSync ||
-        self.requestFileSystemSync;
+var TEMPORARY = 0
 
-    var fs;
+module.exports = function () {
+  var requestFileSystemSync = self.webkitRequestFileSystemSync ||
+    self.requestFileSystemSync
 
-    function makeRequest(url) {
-        try {
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', url, false); // Note: synchronous
-            xhr.responseType = 'blob';
-            xhr.send();
-            return xhr.response;
-        } catch (e) {
-            return onError(e);
-        }
+  var fs
+
+  function makeRequest (url) {
+    try {
+      var xhr = new XMLHttpRequest()
+      xhr.open('GET', url, false) // Note: synchronous
+      xhr.responseType = 'blob'
+      xhr.send()
+      return xhr.response
+    } catch (e) {
+      return onError(e)
+    }
+  }
+
+  function onError (e) {
+    postMessage({
+      error: 'ERROR: ' + e.toString()
+    })
+  }
+
+  onmessage = function (e) {
+    var data = e.data
+
+    // Make sure we have the right parameters.
+    if (!data.fileName || !data.url) {
+      return
     }
 
-    function onError(e) {
+    try {
+      if (!fs) {
+        fs = requestFileSystemSync(TEMPORARY, 500 * 1024 * 1024 /* 500MB */)
+      }
+
+      var fileEntry = fs.root.getFile(data.fileName, {
+        create: true
+      })
+
+      var blob = makeRequest(data.url)
+
+      try {
+        fileEntry.createWriter().write(blob)
         postMessage({
-            error: 'ERROR: ' + e.toString()
-        });
+          id: data.id,
+          url: fileEntry.toURL()
+        })
+      } catch (e) {
+        onError(e)
+      }
+    } catch (e) {
+      onError(e)
     }
-
-    onmessage = function(e) {
-        var data = e.data;
-
-        // Make sure we have the right parameters.
-        if (!data.fileName || !data.url) {
-            return;
-        }
-
-        try {
-            if (!fs) {
-                fs = requestFileSystemSync(TEMPORARY, 500 * 1024 * 1024 /*500MB*/ );
-            }
-
-            var fileEntry = fs.root.getFile(data.fileName, {
-                create: true
-            });
-
-            var blob = makeRequest(data.url);
-
-            try {
-                fileEntry.createWriter().write(blob);
-                postMessage({
-                    id: data.id,
-                    url: fileEntry.toURL()
-                });
-            } catch (e) {
-                onError(e);
-            }
-
-        } catch (e) {
-            onError(e);
-        }
-    };
-
-};
+  }
+}
